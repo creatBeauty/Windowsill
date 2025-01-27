79c7a547b48ff53bf46aeb5391b2af76be5536fb
AleX-09-13-svg <_email@example.com>
2025-01-27 11:48:48 +0300

    new

--- index.js
+++
@@ -1,9 +0,0 @@
-import { Carousel } from './modules/carousel.js';
-import { Form } from './modules/form.js';
-import { EmailService } from './modules/emailService.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const emailService = new EmailService();
-  const carousel = new Carousel();
-  const form = new Form(emailService);
-});
