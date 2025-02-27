a86308ce8e5fd6be7c8f945193918e6883944389
AleX-09-13-svg <_email@example.com>
2025-02-27 23:41:40 +0300

    14

--- src/Buttons to Display/13LocationButton.js
+++
@@ -1,9 +0,0 @@
-import { Map } from '../logic/13Map.js';
-
-function _13openMap() {
-  const mapButton = document.getElementById('openMap');
-
-  mapButton.addEventListener('click', Map);
-}
-
-export { _13openMap };
