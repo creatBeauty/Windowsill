134b1f20f867b79eb48d03f541f3d62ddddcef57
AleX-09-13-svg <_email@example.com>
2025-02-27 03:09:05 +0300

    6

--- src/logic/cutting.js
+++ src/logic/cutting.js
@@ -5,8 +5,7 @@
 const SHEET_WIDTH = 760;
 const SHEET_HEIGHT = 3660;
 const SPACING = 15;
-const SHEET_GAP = 100; // Отступ для деталей
-const SHEET_VISUAL_GAP = -2000; // Визуальный отступ между листами
+const SHEET_GAP = 20; // Уменьшено до 20px для минимального расстояния между листами
 const PART_SPACING = 20; // Отступ для деталей
 const SCALE_FACTOR = 4; // Коэффициент масштабирования визуализации
 let SHEETS_COUNT = 0; // Начальное количество листов
